e63a91fb4249e65c67dc7dd45a0c5a0168d86be9
muneebhashone <themuneebh@gmail.com>
2025-01-20 09:04:18 +0500

    fix: axios api urls for server and client

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/v1/:path*/',
+        destination: process.env.NEXT_PUBLIC_SERVER_URL + '/:path*',
+      },
+      {
+        source: '/api/v1/:path*',
+        destination: process.env.NEXT_PUBLIC_SERVER_URL + '/:path*',
+      }
+    ];
+  },
 };
 
 export default nextConfig;
